d1e2a059fa86a7b2a885e0d55f37a779235cd663
StefanMeester <stefanmeester89@gmail.com>
2026-04-30 21:51:08 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,10 @@
-
+const cursor = document.getElementById('cursor');
+document.addEventListener('mousemove', (e) => {
+cursor.style.left = e.clientX + 'px';
+cursor.style.top = e.clientY + 'px';
+});
+// Cursor vergroten bij hovers
+document.querySelectorAll('a, button, .card').forEach(el => {
+el.addEventListener('mouseenter', () => cursor.classList.add('cursor-hover'));
+el.addEventListener('mouseleave', () => cursor.classList.remove('cursor-hover'));
+});
